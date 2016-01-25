ded8eebc27df02eb1dc064ddceb66104a1bb96fc
Ben Densmore <bdensmore@densmore.net>
2016-01-25 21:09:42 +0000

    Fix packaging of init

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 var request_init = require('./Request/init.js');
 var key;
 
-function init(service, securityPacket, secret, requestPacket, action) {
-  try {
-      key = request_init(service, securityPacket, secret, requestPacket, action);
-  } catch(e) {
-      key = 'There was an error ' + e.message;
-  }
+var init = {
+  function (service, securityPacket, secret, requestPacket, action) {
+    try {
+        key = request_init(service, securityPacket, secret, requestPacket, action);
+      } catch(e) {
+        key = 'There was an error ' + e.message;
+      }
 
-  return key;
+      return key;
+    }
 }
+
+modules.export = init;
